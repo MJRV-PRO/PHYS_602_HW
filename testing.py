65f5d6cc797271d837b58f9d5f37fc011d4f7614
Marlon Josue Rivera Valladares <marlon.riveravalladares@mail.mcgill.ca>
2025-02-28 09:45:24 -0500

    Added Documentation and fixed a bug

--- testing.py
+++ testing.py
@@ -1,4 +1,17 @@
 import numpy as np
 
 def randomNumberGenerator(bounds):
-  return np.random(bounds[0],bounds[1])
+  """
+  Returns a random integer between the bounds given. Uses the numpy random.
+
+  Parameters
+  ----------
+  bounds : 2D tuple
+  A tuple containing the lower and upper bounds for the random number.
+
+  Returns
+  -------
+  np.random.randint(bounds[0],bounds[1]) : int
+  A random integer.
+""" 
+return np.random.randint(bounds[0],bounds[1])
